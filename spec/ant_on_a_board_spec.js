e7d61760f6a18a2a20f4967a9caf991deaea64fe
John Maxwell <jmax@jmaxhome.com>
2015-11-08 17:22:14 -0500

    Refactoring(specs): extract method displayed_cell()

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -1,16 +1,20 @@
 describe("a board with an ant on it, when displayed", function() {
     var the_ant= null;
-    var display_table= null;
+    var displayed_table= null;
+
+    function displayed_cell(row, column) {
+	return $(displayed_table[0].rows[row].cells[column]);
+    }
 
     beforeEach(function() {
     	var the_board= new Board();
     	the_ant= the_board.add_ant();
 	jasmine.getFixtures().set("<div id='board-div'></div>");
 	the_board.display($('#board-div'));
-	display_table= $('#board-div>table#ant-board');
+	displayed_table= $('#board-div>table#ant-board');
     });
 
     it("puts the ant image in the ant's cell", function() {
-	expect($(display_table[0].rows[the_ant.row()].cells[the_ant.column()])).toHaveHtml("<img src=\"images/ant-up.jpg\">");
+	expect(displayed_cell(the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"images/ant-up.jpg\">");
     });
 });
