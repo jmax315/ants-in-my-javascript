abe7c626de93452d73ed805d34c57fb97d3555de
John Maxwell <jmax@jmaxhome.com>
2015-11-08 13:47:58 -0500

    Spec passes.

--- src/board.js
+++ src/board.js
@@ -15,6 +15,7 @@ Board.prototype.color= function(row, column) {
 
 Board.prototype.display= function(container) {
     var displayed_board= $("<table id='ant-board'></table>").appendTo(container);
-    for (var i= 0; i < 10; i++)
-	displayed_board.append("<tr></tr");
+    for (var i= 0; i < 10; i++) {
+	displayed_board.append("<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+    }
 }
