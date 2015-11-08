b8240270342075b260958cef34baca7421a9de70
John Maxwell <jmax@jmaxhome.com>
2015-11-08 16:09:31 -0500

    Spec for new ant's position.

---
+++ spec/ant_on_a_board_spec.js
@@ -0,0 +1,13 @@
+describe("an ant on the board", function() {
+    var the_board= null;
+    var the_ant= null;
+
+    beforeEach(function() {
+    	the_board= new Board();
+    	the_ant= the_board.add_ant();
+    });
+
+    it("is located at [5,5]", function() {
+	expect(the_ant.location()).toEqual([5,5]);
+    });
+});
