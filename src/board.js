7a4f340bc26523e566ef0694459c4022f7d60239
John Maxwell <jmax@jmaxhome.com>
2015-11-08 13:54:05 -0500

    Refactoring: removed duplication.

--- src/board.js
+++ src/board.js
@@ -16,6 +16,8 @@ Board.prototype.color= function(row, column) {
 Board.prototype.display= function(container) {
     var displayed_board= $("<table id='ant-board'></table>").appendTo(container);
     for (var i= 0; i < 10; i++) {
-	var displayed_row= $("<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>").appendTo(displayed_board);
+	var displayed_row= $("<tr></tr>").appendTo(displayed_board);
+	for (var j= 0; j < 10; j++)
+	    $("<td></td>").appendTo(displayed_row);
     }
 }
