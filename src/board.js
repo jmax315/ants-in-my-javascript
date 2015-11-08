3fd33edfe9649723fe32d50b9e1ed9f49dbe5416
John Maxwell <jmax@jmaxhome.com>
2015-11-08 16:57:03 -0500

    First try at making spec pass.

--- src/board.js
+++ src/board.js
@@ -13,20 +13,23 @@ Board.prototype.color= function(row, column) {
     return 'white';
 }
 
-Board.prototype.display_cell= function(displayed_row) {
+Board.prototype.display_cell= function(displayed_row, row, column) {
+    if (row === 5 && column === 5)
 	$("<td class='white-cell'></td>").appendTo(displayed_row);
+    else
+	$("<td class='white-cell'><img src=\"images/ant-up.jpg\"></td>").appendTo(displayed_row);
 }
 
-Board.prototype.display_row= function(displayed_board) {
+Board.prototype.display_row= function(displayed_board, row) {
     var displayed_row= $("<tr></tr>").appendTo(displayed_board);
     for (var column= 0; column < this.width(); column++)
-	this.display_cell(displayed_row);
+	this.display_cell(displayed_row, row, column);
 }
 
 Board.prototype.display= function(container) {
     var displayed_board= $("<table id='ant-board'></table>").appendTo(container);
     for (var row= 0; row < this.height(); row++)
-	this.display_row(displayed_board);
+	this.display_row(displayed_board, row);
 }
 
 Board.prototype.add_ant= function() {
