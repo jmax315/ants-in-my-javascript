85e9eba421bce90e567a53bc926c932275038242
John Maxwell <jmax@jmaxhome.com>
2015-11-08 14:09:03 -0500

    Refactoring: extract method.

--- src/board.js
+++ src/board.js
@@ -13,11 +13,14 @@ Board.prototype.color= function(row, column) {
     return 'white';
 }
 
+Board.prototype.display_row= function(displayed_board) {
+    var displayed_row= $("<tr></tr>").appendTo(displayed_board);
+    for (var column= 0; column < this.width(); column++)
+	$("<td></td>").appendTo(displayed_row);
+}
+
 Board.prototype.display= function(container) {
     var displayed_board= $("<table id='ant-board'></table>").appendTo(container);
-    for (var row= 0; row < 10; row++) {
-	var displayed_row= $("<tr></tr>").appendTo(displayed_board);
-	for (var column= 0; column < 10; column++)
-	    $("<td></td>").appendTo(displayed_row);
-    }
+    for (var row= 0; row < this.height(); row++)
+	this.display_row(displayed_board);
 }
