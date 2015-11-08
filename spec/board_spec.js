b835f63d15b5854dfa7db1e33dae64adb63d34b8
John Maxwell <jmax@jmaxhome.com>
2015-11-08 17:33:56 -0500

    Spec cleanup: collapsed nested describes

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -23,36 +23,31 @@ describe("A 10x10 board", function() {
 		expect(the_board.color(row, column)).toEqual('white', cell_message(row, column));
     });
 
-    describe("when displayed", function() {
+    describe("when displayed, creates a table", function() {
+	var displayed_table= null;
+
 	beforeEach(function() {
 	    jasmine.getFixtures().set("<div id='board-div'></div>");
 	    the_board.display($('#board-div'));
+	    displayed_table= $('#board-div>table#ant-board');
 	});
 
-	describe("creates a table", function() {
-	    var displayed_table= null;
+	it("in the DOM", function() {
+	    expect(displayed_table).toBeInDOM();
+	}); 
 
-	    beforeEach(function() {
-		displayed_table= $('#board-div>table#ant-board');
-	    });
+	it("with 10 rows", function() {
+	    expect(displayed_table[0].rows.length).toEqual(10);
+	});
 
-	    it("in the DOM", function() {
-		expect(displayed_table).toBeInDOM();
-	   }); 
+	it("with 10 columns", function() {
+	    expect(displayed_table[0].rows[0].cells.length).toEqual(10);
+	});
 
-	    it("with 10 rows", function() {
-		expect(displayed_table[0].rows.length).toEqual(10);
-	    });
-
-	    it("with 10 columns", function() {
-		expect(displayed_table[0].rows[0].cells.length).toEqual(10);
-	    });
-
-	    it("with each cell having a CSS class of 'white-cell'", function() {
-		for (var row= 0; row < 10; row++)
-		    for (var column= 0; column < 10; column++)
-			expect(displayed_cell(displayed_table, row, column)).toHaveClass('white-cell', cell_message(row, column));
-	    });
+	it("with each cell having a CSS class of 'white-cell'", function() {
+	    for (var row= 0; row < 10; row++)
+		for (var column= 0; column < 10; column++)
+		    expect(displayed_cell(displayed_table, row, column)).toHaveClass('white-cell', cell_message(row, column));
 	});
     });
     
