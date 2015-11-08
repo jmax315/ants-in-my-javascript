f370878f2c2f2f4242c821f6928a66c93e2e670a
John Maxwell <jmax@jmaxhome.com>
2015-11-08 18:43:05 -0500

    Refactoring: move display_cell() from Board to Ui

--- src/board.js
+++ src/board.js
@@ -13,13 +13,6 @@ Board.prototype.cell_color= function(row, column) {
     return 'white';
 }
 
-Board.prototype.display_cell= function(displayed_row, row, column) {
-    if (row === 5 && column === 5)
-	$("<td class='white-cell'><img src=\"images/ant-up.jpg\"></td>").appendTo(displayed_row);
-    else
-	$("<td class='white-cell'></td>").appendTo(displayed_row);
-}
-
 Board.prototype.display= function(container) {
     var ui= new Ui(container);
     var displayed_board= ui.display(this);
