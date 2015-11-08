12ce4f66f82cde7ca19b305edf3335230c540319
John Maxwell <jmax@jmaxhome.com>
2015-11-08 17:48:50 -0500

    Spec cleanup: use helper display_board(); add afterEach for cleanup

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -5,12 +5,15 @@ describe("a board with an ant on it, when displayed", function() {
     beforeEach(function() {
     	var the_board= new Board();
     	the_ant= the_board.add_ant();
-	jasmine.getFixtures().set("<div id='board-div'></div>");
-	the_board.display($('#board-div'));
-	displayed_table= $('#board-div>table#ant-board');
+	displayed_table= display_table(the_board);
     });
 
     it("puts the ant image in the ant's cell", function() {
 	expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"images/ant-up.jpg\">");
+    });
+
+    afterEach(function() {
+	the_ant= null;
+	displayed_table= null;
     });
 });
