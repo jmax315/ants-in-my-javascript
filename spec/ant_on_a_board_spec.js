2f92297f17296decc53a17ac52414d78a91c6225
John Maxwell <jmax@jmaxhome.com>
2015-11-08 23:51:08 -0500

    Added display spec for an ant after stepping.

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -17,3 +17,24 @@ describe("a board with an ant on it, when displayed", function() {
 	displayed_table= null;
     });
 });
+
+describe("a board with an ant, after stepping, when displayed", function() {
+    var the_ant= null;
+    var displayed_table= null;
+
+    beforeEach(function() {
+    	var the_board= new Board();
+    	the_ant= the_board.add_ant();
+	the_ant.step();
+	displayed_table= display_table(the_board);
+    });
+
+    it("puts the ant image in the ant's cell", function() {
+	expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"images/ant-up.jpg\">");
+    });
+
+    afterEach(function() {
+	the_ant= null;
+	displayed_table= null;
+    });
+});
