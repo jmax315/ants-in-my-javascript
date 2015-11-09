247bf68f9649c4041e792e1253b621c32ca453c0
John Maxwell <jmax@jmaxhome.com>
2015-11-08 21:39:59 -0500

    Paramaterized ant step specs on initial direction.

--- spec/ant_step_spec.js
+++ spec/ant_step_spec.js
@@ -1,26 +1,35 @@
-describe("after stepping an ant at [5,5], facing up, on a white cell", function() {
-    var the_board= null;
-    var the_ant= null;
+_.each(
+    [{direction: 'up',    expected_row: 5, expected_column: 6, expected_direction: 'right'},
+     {direction: 'left',  expected_row: 4, expected_column: 5, expected_direction: 'up'},
+     {direction: 'down',  expected_row: 5, expected_column: 4, expected_direction: 'left'},
+     {direction: 'right', expected_row: 6, expected_column: 5, expected_direction: 'down'}],
 
-    beforeEach(function() {
-	the_board= new Board();
-	the_ant= the_board.add_ant();
-	the_ant.step();
-    });
+    function(test_case) {
+	describe("after stepping an ant at [5,5], on a white cell, facing " + test_case.direction, function() {
+	    var the_board= null;
+	    var the_ant= null;
 
-    it("is located at row 5", function() {
-	expect(the_ant.row()).toEqual(5);
-    });
+	    beforeEach(function() {
+		the_board= new Board();
+		the_ant= the_board.add_ant();
+		the_ant.direction(test_case.direction);
+		the_ant.step();
+	    });
 
-    it("is located at column 6", function() {
-	expect(the_ant.column()).toEqual(6);
-    });
+	    it("is located at row " + test_case.expected_row, function() {
+		expect(the_ant.row()).toEqual(test_case.expected_row);
+	    });
 
-    it("is facing right", function() {
-	expect(the_ant.direction()).toEqual('right');
-    });
+	    it("is located at column " + test_case.expected_column, function() {
+		expect(the_ant.column()).toEqual(test_case.expected_column);
+	    });
 
-    it("its old cell is flipped to black", function() {
-	expect(the_board.cell_color(5, 5)).toEqual('black');
+	    it("is facing " + test_case.expected_direction, function() {
+		expect(the_ant.direction()).toEqual(test_case.expected_direction);
+	    });
+
+	    it("its old cell is flipped to black", function() {
+		expect(the_board.cell_color(5, 5)).toEqual('black');
+	    });
+	});
     });
-});
