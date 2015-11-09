74a544165f98c59a0d4bf85b781435ee9c96da61
John Maxwell <jmax@jmaxhome.com>
2015-11-09 12:34:49 -0500

    Added specs for range checking in Board.cell_color()

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -1,4 +1,4 @@
-describe("A 10x10 board", function() {
+describe("A default board", function() {
     var the_board= null;
     
     beforeEach(function() {
@@ -17,6 +17,34 @@ describe("A 10x10 board", function() {
 	for (var row= 0; row < the_board.height(); row++)
 	    for (var column= 0; column < the_board.width(); column++)
 		expect(the_board.cell_color(row, column)).toEqual('white', cell_message(row, column));
+    });
+
+    afterEach(function() {
+	the_board= null;
+    });
+});
+
+describe("Board.cell_color()", function() {
+    var the_board= null;
+    
+    beforeEach(function() {
+	the_board= new Board();
+    });
+
+    it("does not barf when passed a row that's too small", function() {
+	expect(function() {the_board.cell_color(-1, 0);}).not.toThrow();
+    });
+
+    it("does not barf when passed a column that's too small", function() {
+	expect(function() {the_board.cell_color(0, -1);}).not.toThrow();
+    });
+
+    it("does not barf when passed a row that's too large", function() {
+	expect(function() {the_board.cell_color(the_board.height(), 0);}).not.toThrow();
+    });
+
+    it("does not barf when passed a column that's too large", function() {
+	expect(function() {the_board.cell_color(0, the_board.width());}).not.toThrow();
     });
 
     afterEach(function() {
