bd7c125b52151cb625436a5acc25b6bcf9c9e67d
John Maxwell <jmax@jmaxhome.com>
2015-11-08 19:53:40 -0500

    Spec cleanup: moved display specs from board specs out to new ui specs

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -19,36 +19,6 @@ describe("A 10x10 board", function() {
 		expect(the_board.cell_color(row, column)).toEqual('white', cell_message(row, column));
     });
 
-    describe("when displayed, creates a table", function() {
-	var displayed_table= null;
-
-	beforeEach(function() {
-	    displayed_table= display_table(the_board);
-	});
-
-	it("in the DOM", function() {
-	    expect(displayed_table).toBeInDOM();
-	}); 
-
-	it("with 10 rows", function() {
-	    expect(displayed_table[0].rows.length).toEqual(the_board.height());
-	});
-
-	it("with 10 columns", function() {
-	    expect(displayed_table[0].rows[0].cells.length).toEqual(the_board.width());
-	});
-
-	it("with each cell having a CSS class of 'white-cell'", function() {
-	    for (var row= 0; row < the_board.height(); row++)
-		for (var column= 0; column < the_board.width(); column++)
-		    expect(displayed_cell(displayed_table, row, column)).toHaveClass('white-cell', cell_message(row, column));
-	});
-
-	afterEach(function() {
-	    displayed_table= null;
-	});
-    });
-    
     afterEach(function() {
 	the_board= null;
     });
