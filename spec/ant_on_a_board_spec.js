8728bdd811e17d0d58a7538334c8696e6d3372b6
John Maxwell <jmax@jmaxhome.com>
2015-11-09 11:30:29 -0500

    Made spec explicit about board square and ant direction.
    Also changed the desired ant image file name; the ui-spike showed that the image is going to have to vary with cell color, as well as orientation.

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -1,15 +1,19 @@
-describe("a board with an ant on it, when displayed", function() {
+describe("a board with an ant on a white cell, facing up, when displayed", function() {
     var the_ant= null;
     var displayed_table= null;
 
     beforeEach(function() {
     	var the_board= new Board();
     	the_ant= the_board.add_ant();
+
+	the_ant.direction('up');
+	the_board.cell_color(the_ant.row(), the_ant.column(), 'white');
+
 	displayed_table= display_table(the_board);
     });
 
     it("puts the ant image in the ant's cell", function() {
-	expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"images/ant-up.jpg\">");
+	expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"images/ant-on-white-up.jpg\">");
     });
 
     afterEach(function() {
