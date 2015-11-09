da926905ce7d756c1c1ef8d84a0f6b0cd5b9a7e9
John Maxwell <jmax@jmaxhome.com>
2015-11-09 11:49:39 -0500

    Added test cases for ant on a black square.

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -4,6 +4,11 @@ _.each(
 	{color: 'white', direction: 'left',  expected_image: "images/ant-on-white-left.jpg"},
 	{color: 'white', direction: 'down',  expected_image: "images/ant-on-white-down.jpg"},
 	{color: 'white', direction: 'right', expected_image: "images/ant-on-white-right.jpg"},
+
+	{color: 'black', direction: 'up',    expected_image: "images/ant-on-black-up.jpg"},
+	{color: 'black', direction: 'left',  expected_image: "images/ant-on-black-left.jpg"},
+	{color: 'black', direction: 'down',  expected_image: "images/ant-on-black-down.jpg"},
+	{color: 'black', direction: 'right', expected_image: "images/ant-on-black-right.jpg"},
     ],
     function(test_case) {
 	describe("a board with an ant on a " + test_case.color + " cell, facing " + test_case.direction + ", when displayed", function() {
