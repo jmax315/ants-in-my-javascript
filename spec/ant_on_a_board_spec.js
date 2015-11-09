f5395c5a0606378d1e6fef5844dca963c26fec71
John Maxwell <jmax@jmaxhome.com>
2015-11-09 11:41:00 -0500

    Paramaterized ant on a board spec.

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -1,24 +1,30 @@
-describe("a board with an ant on a white cell, facing up, when displayed", function() {
-    var the_ant= null;
-    var displayed_table= null;
+_.each(
+    [
+	{color: 'white', direction: 'up', expected_image: "images/ant-on-white-up.jpg"},
+    ],
+    function(test_case) {
+	describe("a board with an ant on a " + test_case.color + " cell, facing " + test_case.direction + ", when displayed", function() {
+	    var the_ant= null;
+	    var displayed_table= null;
 
-    beforeEach(function() {
-    	var the_board= new Board();
-    	the_ant= the_board.add_ant();
+	    beforeEach(function() {
+    		var the_board= new Board();
+    		the_ant= the_board.add_ant();
 
-	the_ant.direction('up');
-	the_board.cell_color(the_ant.row(), the_ant.column(), 'white');
+		the_ant.direction(test_case.color);
+		the_board.cell_color(the_ant.row(), the_ant.column(), test_case.color);
 
-	displayed_table= display_table(the_board);
+		displayed_table= display_table(the_board);
+	    });
+
+	    it("puts the correct image in the ant's cell", function() {
+		expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"" + test_case.expected_image + "\">");
+	    });
+
+	    afterEach(function() {
+		the_ant= null;
+		displayed_table= null;
+	    });
+	});
     });
-
-    it("puts the ant image in the ant's cell", function() {
-	expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"images/ant-on-white-up.jpg\">");
-    });
-
-    afterEach(function() {
-	the_ant= null;
-	displayed_table= null;
-    });
-});
 
