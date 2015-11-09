692d178d60a124b89def9c671578c78c07d090a5
John Maxwell <jmax@jmaxhome.com>
2015-11-08 20:17:35 -0500

    Added spec for stepping an ant at [5,5], facing up, on a white cell

---
+++ spec/ant_step_spec.js
@@ -0,0 +1,12 @@
+describe("after stepping an ant at [5,5], facing up, on a white cell", function() {
+    var the_ant= null;
+
+    beforeEach(function() {
+	the_ant= new Ant();
+	the_ant.step();
+    });
+
+    it("is located at row 5", function() {
+	expect(the_ant.row()).toEqual(5);
+    });
+});
