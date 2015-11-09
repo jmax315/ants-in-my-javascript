009d65d43647769f5d57ac58caf8de94495d25ce
John Maxwell <jmax@jmaxhome.com>
2015-11-09 00:06:14 -0500

    Extracted Ui.is_ant_at()

--- src/ui.js
+++ src/ui.js
@@ -8,22 +8,28 @@ accessor(Ui, "container");
 accessor(Ui, "board");
 accessor(Ui, "displayed_board");
 
+Ui.prototype.is_ant_at= function(row, column) {
+    return this.board().ant() &&
+	row === this.board().ant().row() &&
+	column === this.board().ant().column();
+};
+
 Ui.prototype.display_cell= function(displayed_row, row, column) {
-    if (this.board().ant() && row === this.board().ant().row() && column === this.board().ant().column())
+    if (this.is_ant_at(row, column))
 	$("<td class='white-cell'><img src=\"images/ant-up.jpg\"></td>").appendTo(displayed_row);
     else
 	$("<td class='white-cell'></td>").appendTo(displayed_row);
-}
+};
 
 Ui.prototype.display_row= function(a_board, row) {
     var displayed_row= $("<tr></tr>").appendTo(this.displayed_board());
     for (var column= 0; column < this.board().width(); column++)
 	this.display_cell(displayed_row, row, column);
-}
+};
 
 Ui.prototype.display= function(a_board) {
     this.board(a_board);
     this.displayed_board($("<table id='ant-board'></table>").appendTo(this.container()));
     for (var row= 0; row < this.board().height(); row++)
 	this.display_row(this.board(), row);
-}
+};
