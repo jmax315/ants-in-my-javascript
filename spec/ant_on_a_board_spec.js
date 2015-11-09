03b224648b07892edc0919a97a39a574010d6bbc
John Maxwell <jmax@jmaxhome.com>
2015-11-09 13:13:00 -0500

    Improved spec descriptions

--- spec/ant_on_a_board_spec.js
+++ spec/ant_on_a_board_spec.js
@@ -11,7 +11,7 @@ _.each(
 	{color: 'black', direction: 'right', expected_image: "images/ant-on-black-right.jpg"},
     ],
     function(test_case) {
-	describe("a board with an ant on a " + test_case.color + " cell, facing " + test_case.direction + ", when displayed", function() {
+	describe("when a board with an ant on a " + test_case.color + " cell, facing " + test_case.direction + " is displayed", function() {
 	    var the_ant= null;
 	    var displayed_table= null;
 
@@ -25,7 +25,7 @@ _.each(
 		displayed_table= display_table(the_board);
 	    });
 
-	    it("puts the correct image in the ant's cell", function() {
+	    it("puts " + test_case.expected_image + " in the ant's cell", function() {
 		expect(displayed_cell(displayed_table, the_ant.row(), the_ant.column())).toHaveHtml("<img src=\"" + test_case.expected_image + "\">");
 	    });
 
