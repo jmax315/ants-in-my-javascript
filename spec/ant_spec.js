9e05239414f69d8a0e1fe0f2be353bb987d10a3f
John Maxwell <jmax@jmaxhome.com>
2015-11-09 13:01:00 -0500

    Added specs for Ant.step() when the ant is off the board.

--- spec/ant_spec.js
+++ spec/ant_spec.js
@@ -17,3 +17,40 @@ describe("an ant with default values", function() {
 	expect(the_ant.direction()).toEqual('up');
     });
 });
+
+describe("Ant.step()", function() {
+    var the_board= null;
+    var the_ant= null;
+
+    function step() {the_ant.step();}
+
+    beforeEach(function() {
+	the_board= new Board();
+	the_ant= the_board.add_ant();
+    });
+
+    it("does not barf when the ant is off to the left of the board", function() {
+	the_ant.column(-1);
+	expect(step).not.toThrow();
+    });
+
+    it("does not barf when the ant is off to the right of the board", function() {
+	the_ant.column(the_board.width());
+	expect(step).not.toThrow();
+    });
+
+    it("does not barf when the ant is off above the board", function() {
+	the_ant.row(-1);
+	expect(step).not.toThrow();
+    });
+
+    it("does not barf when the ant is off below the board", function() {
+	the_ant.row(the_board.height());
+	expect(step).not.toThrow();
+    });
+
+    afterEach(function() {
+	the_ant= null;
+	the_board= null;
+    });
+});
