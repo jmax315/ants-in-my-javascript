46699576916a69c96b41989ac0ef7a324bc460ae
John Maxwell <jmax@jmaxhome.com>
2015-11-08 20:18:45 -0500

    Spec passes.

--- src/ant.js
+++ src/ant.js
@@ -3,12 +3,15 @@ function Ant() {
 
 Ant.prototype.row= function() {
     return 5;
-}
+};
 
 Ant.prototype.column= function() {
     return 5;
-}
+};
 
 Ant.prototype.direction= function() {
     return 'up';
-}
+};
+
+Ant.prototype.step= function() {
+};
