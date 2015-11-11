af278f38b347b2d4e8eab65d60beb897229e42ef
John Maxwell <jmax@jmaxhome.com>
2015-11-11 07:04:01 -0500

    First spec for App.initialize()

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,4 +1,4 @@
-describe("an App", function() {
+describe("a newly created App", function() {
     var the_app= null;
 
     beforeEach(function() {
@@ -6,14 +6,28 @@ describe("an App", function() {
 	the_app= new App($('#app-container'));
     });
     
-    describe("when newly created", function() {
-	it ("knows its container", function() {
-	    expect(the_app.container()).toEqual($('#app-container'));
-	});
+    it ("knows its container", function() {
+	expect(the_app.container()).toEqual($('#app-container'));
+    });
 
-	it ("does not put anything into its container", function() {
-	    expect(the_app.container()).toBeEmpty();
+    it ("does not put anything into its container", function() {
+	expect(the_app.container()).toBeEmpty();
 
-	});
     });
 });
+
+describe("App.initialize()", function() {
+    var the_app= null;
+
+    beforeEach(function() {
+	jasmine.getFixtures().set("<div id='app-container'></div>");
+	the_app= new App($('#app-container'));
+
+	the_app.initialize();
+    });
+    
+    it("puts a board into its container", function() {
+	expect($('#app-container>table')).toBeVisible();
+    });
+});
+
