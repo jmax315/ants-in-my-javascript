be221115060b43a28bb5b70e396553f1224662e0
John Maxwell <jmax@jmaxhome.com>
2015-11-11 07:04:14 -0500

    Specs pass.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,9 @@ function App(app_container) {
 App.prototype.container= function() {
     return this._container;
 };
+
+App.prototype.initialize= function() {
+    var the_board= new Board();
+    var board_drawer= new BoardDrawer(this.container());
+    board_drawer.display(the_board);
+};
